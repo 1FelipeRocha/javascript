17f59dd2c8052b29cdef5edbc500b891b454985f
1FelipeRocha <felipe_rocha2708@hotmail.com>
2023-02-13 21:37:28 -0300

    aulas e desafio

---
+++ aula12/ex0111.js
@@ -0,0 +1,8 @@
+var país = 'Rio de Janeiro'
+console.log (`Se voce nasceu em ${país}.`)
+if        (país =='Brasil')    {console.log ('Voce é brasileiro')} 
+else if   (país =='EUA')       {console.log ('voce é americano')} 
+else if   (país =='India')     {console.log ('voce é indiano')}
+else if   (país =='Canada')    {console.log ('voce é canadense')}
+else if   (país =='Rio de Janeiro') {console.log('voce é carioca igual ao Marcio')}
+else                              {console.log('voce é estrangeiro')}
