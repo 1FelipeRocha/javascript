50e139a66d1fa86f52220b088834854ccf703574
1FelipeRocha <felipe_rocha2708@hotmail.com>
2023-02-15 19:15:18 -0300

    fim do exercicio

--- desafio01/script.js
+++ desafio01/script.js
@@ -6,9 +6,12 @@ var hora = data.getHours()
 msg.innerHTML = `Agora são ${hora} horas.`
 if (hora >= 0 && hora <12) {
     img.src = "imagens/manha.png"
+    document.body.style.background = '#e2cd9f'
 } else if (hora >= 12 && hora <18) {
-    img.src = "imagens/tarde.png"
+    img.src = "imagens/pexels-george-desipris-858241.jpg"
+    document.body.style.background = '#b98464'
 } else {
-    img.src = "imagens/noite.png"
+    img.src = "imagens/pexels-eberhard-grossgasteiger-1421903.jpg"
+    document.body.style.background = '#515154'
 }
 }
