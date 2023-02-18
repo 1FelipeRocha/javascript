e1397bbe4fbf7ba1845e74ebb1d1173bb7ae8d5b
1FelipeRocha <felipe_rocha2708@hotmail.com>
2023-02-18 08:57:54 -0300

    desafio 04

--- desafio04/script.js
+++ desafio04/script.js
@@ -1,40 +1,18 @@
-function verificar() {
-    var data = new Date()
-    var ano = data.getFullYear()
-    var fano = document.getElementById('txtano')
-    var res = document.getElementById('res')
-    if (fano.value.length == 0 || Number(fano.value) > ano) {
-        window.alert('[ERRO] Verifique os dados e tente novamente')
+function tabuada () {
+    var num = document.getElementById('txtn')
+    var tab = document.getElementById('seltab')
+    if (num.value.length == 0) {
+        window.alert('Por favor,digite um número!')
     } else {
-        var fsex = document.getElementsByName('radsex')
-        var idade = ano - Number(fano.value)
-        var genero = ''
-        var img = document.createElement('img')
-        img.setAttribute('id', 'foto')
-        if (fsex[0].checked) {
-            genero = 'Homem'
-            if (idade >=0 && idade < 10) {
-                img.setAttribute('src', 'foto-bebe-m.png' )
-            } else if (idade < 21) {
-                img.setAttribute('src', 'foto-jovem-m.png')
-            } else if (idade < 50) {
-                img.setAttribute('src', 'foto-adulto-m.png')
-            } else {
-                img.setAttribute('src', 'foto-idoso-m.png')
-            }
-      } else if (fsex[1].checked) {
-        genero = 'Mulher'
-        if (idade >=0 && idade <10) {
-            img.setAttribute('src', 'foto-bebe-f.png')
-        } else if (idade < 21) {
-            img.setAttribute('src', 'foto-jovem-f.png')
-        } else if (idade < 50) {
-            img.setAttribute('src', 'foto-adulto-f.png')
-        } else {
-            img.setAttribute('src', 'foto-idoso-f.png')
-      }}
-      res.style.textAlign = 'center'
-      res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
-      res.appendChild(img)
+        var n = Number(num.value)
+        var c = 1
+        tab.innerHTML = ''
+        while (c <= 10) {
+            var item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value=`tab${c}`
+            tab.appendChild(item)
+            c++
+        }
     }
 }
