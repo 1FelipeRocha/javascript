0bc6095100bbb16f948d958b8a9dad839fdc1729
1FelipeRocha <felipe_rocha2708@hotmail.com>
2023-03-09 20:49:34 -0300

    ex006

--- Daciuk/challenge06/ex006.js
+++ Daciuk/challenge06/ex006.js
@@ -15,3 +15,30 @@ var a = 20;
 while (a <= 31){
     console.log(a++);
 }
+
+function converToHex (cor) {
+    var hexa;
+    switch(cor) {
+        case 'azul': 
+        hexa = '#FF0000';
+        break;
+        case 'verde':
+        hexa = '#FF1000';
+        break;
+        case 'preto':
+        hexa = '#FF1100';
+        break;
+        case 'vermelho':
+        hexa = '#FF2000';
+        break;
+        case 'branco':
+        hexa = '#FF0300';
+        break;   
+        default: 
+        return `Não temos o equivalente hexadecimal para ${cor}`;   
+        
+    }
+    return `O hexadecimal para a cor ${cor} é ${hexa}.`;
+}
+
+console.log( converToHex('vermelho'))
