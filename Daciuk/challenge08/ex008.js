14419c82ee1c969f24eda4ca218eb4c5292efb8c
1FelipeRocha <felipe_rocha2708@hotmail.com>
2023-03-13 20:06:42 -0300

    Create ex008.js

---
+++ Daciuk/challenge08/ex008.js
@@ -0,0 +1,9 @@
+var sum = function calculateSum (x,y){
+    return x + y;
+    
+}
+
+console.log (sum(2,3))
+console.log( `A soma de ${x} e ${y} é igual a ${x+y}.`)
+
+console.log(`O nome da função que faz a soma é ${sum.name}.`)
