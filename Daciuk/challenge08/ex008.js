af2852d7375d737051aa5fcec1710c7eda839368
1FelipeRocha <felipe_rocha2708@hotmail.com>
2023-03-14 19:15:08 -0300

    Update ex008.js

--- Daciuk/challenge08/ex008.js
+++ Daciuk/challenge08/ex008.js
@@ -1,10 +1,11 @@
 var sum = function calculateSum (x,y){
     return x + y;
     
-}
+};
 
-console.log (sum(2,3))
-//console.log( `A soma de ${x} e ${y} é igual a ${x+y}.`)
+var x = 10;
+var y = 20;
+console.log ( `A soma de ${x} e ${y} é igual a ${sum(x,y)}.`)
 
 console.log(`O nome da função que faz a soma é ${sum.name}.`)
 
@@ -12,8 +13,6 @@ function showName (){
     return 'Felipe';
 }
 
-var varshowName = function(){
-    showName();
-};
+var varshowName = showName;
 
 console.log(`A função ${varshowName.name} retorna ${varshowName()}.`)
