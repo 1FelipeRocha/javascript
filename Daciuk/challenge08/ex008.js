ba7a1baea4b8abdd46e40e966836e2d64ef7b601
1FelipeRocha <felipe_rocha2708@hotmail.com>
2023-03-15 20:08:25 -0300

    conclusão do ex08

--- Daciuk/challenge08/ex008.js
+++ Daciuk/challenge08/ex008.js
@@ -16,3 +16,67 @@ function showName (){
 var varshowName = showName;
 
 console.log(`A função ${varshowName.name} retorna ${varshowName()}.`)
+
+function calculator(z){
+    function calculator2(a,b){
+       switch(z){
+       case'+': return `Resultado da operação: ${a} ${z} ${b} = ${a + b}`;
+       break;
+       case'-': return `Resultado da operação: ${a} ${z} ${b} = ${a - b}`;
+       break;
+       case'*': return `Resultado da operação: ${a} ${z} ${b} = ${a * b}`;
+       break;
+       case'/': return `Resultado da operação: ${a} ${z} ${b} = ${a / b}`;
+       break;
+       case'%': return `Resultado da operação: ${a} ${z} ${b} = ${a % b}`;
+       break;
+       default: return `Operação inválida`;
+       }
+    }
+    return calculator2;
+}
+
+var sum = calculator('+');
+console.log(sum(2,3))
+
+var subtraction = calculator('-')
+console.log(subtraction(3,2))
+
+var multiplication = calculator('*')
+console.log(multiplication(2,3))
+
+var division = calculator('/')
+console.log(division(3,2))
+
+var mod = calculator ('kaere')
+console.log(mod(2,3))
+
+
+
+
+//VERSÃO DACIUK
+
+/*function calculator(z){
+    function calculator2(a,b){
+        var result;
+       switch(z){
+       case'+': result = a + b;
+       break;
+       case'-': result = a - b;
+       break;
+       case'*': result = a * b;
+       break;
+       case'/': result = a / b;
+       break;
+       case'%': result = a % b;
+       break;
+       default: return `Operação inválida`;
+       }
+       return `Resultado da operação: ${a} ${z} ${b} = ${result}`;
+    };
+    return calculator2;
+}
+
+var sum = calculator('+');
+console.log(sum(2,3))
+*/
