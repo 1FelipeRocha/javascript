109177a8d8717fd69da527090fd4d52bb187d207
1FelipeRocha <felipe_rocha2708@hotmail.com>
2023-03-17 18:51:15 -0300

    fim do ex009

--- Daciuk/challenge09/ex00902.js
+++ Daciuk/challenge09/ex00902.js
@@ -8,5 +8,29 @@ function calculator(x,y){
 var sum = calculator(2,3);
 
 console.log( sum(function(x,y){
-    return x +y;
+    return x + y;
+}));
+
+var subtraction = calculator(1,2);
+
+console.log(subtraction(function(x,y){
+    return x - y;
+}));
+
+var multiplication = calculator(5,5);
+
+console.log(multiplication(function(x,y){
+ return x * y;
+}));
+
+var division = calculator(6,6);
+
+console.log(division(function(x,y){
+    return x / y;
+}));
+
+var mod = calculator (5,5);
+
+console.log(mod(function(x,y){
+    return x % y;
 }));
