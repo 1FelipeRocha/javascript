629c3a15b5231a2612c0e7abab6b378e5d11406a
1FelipeRocha <felipe_rocha2708@hotmail.com>
2023-03-16 21:14:52 -0300

    socorro ex009

--- Daciuk/challenge09/ex00902.js
+++ Daciuk/challenge09/ex00902.js
@@ -1,11 +1,12 @@
 function calculator(x,y){
-    (function(callback){
-        function callback(){
-
-        }
-    })
+    return function(callback){
+        return callback(x,y)
+    };
     
 } 
 
 var sum = calculator(2,3);
 
+console.log( sum(function(x,y){
+    return x +y;
+}));
